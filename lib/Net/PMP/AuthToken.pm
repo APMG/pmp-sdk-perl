cb2abd150fe8b9cc2a8f8ad5974b221ae9821e27
Peter Karman <pkarman@mpr.org>
2013-10-04 10:53:26 -0500

    add missing docs, methods

--- lib/Net/PMP/AuthToken.pm
+++ lib/Net/PMP/AuthToken.pm
@@ -7,15 +7,70 @@ has 'token_type'       => ( is => 'rw', isa => 'Str', required => 1, );
 has 'token_issue_date' => ( is => 'rw', isa => 'Str', required => 1, );
 has 'token_expires_in' => ( is => 'rw', isa => 'Int', required => 1, );
 
+use overload
+    '""'     => sub { $_[0]->as_string; },
+    'bool'   => sub {1},
+    fallback => 1;
+
 __PACKAGE__->meta->make_immutable();
 
 our $VERSION = '0.01';
 
 sub expires_in { shift->token_expires_in(@_) }
 
+sub as_string { return shift->access_token }
+
 1;
 
 __END__
+
+=head1 NAME
+
+Net::PMP::AuthToken - authorization token for Net::PMP::Client
+
+=head1 SYNOPSIS
+
+ use Net::PMP::Client;
+ 
+ my $host = 'https://api-sandbox.pmp.io';
+ my $client_id = 'i-am-a-client';
+ my $client_secret = 'i-am-a-secret';
+
+ # instantiate a client
+ my $client = Net::PMP::Client->new(
+     host   => $host,
+     id     => $client_id,
+     secret => $client_secret,
+ ) or die "Can't connect to server $host: " . $Net::PMP::Client::Error;
+
+ # authenticate
+ my $token = $client->get_token();
+ if ($token->expires_in() < 10) {
+     die "Access token expires too soon. Not enough time to make a request. Mayday, mayday!";
+ }
+ printf("PMP token is: %s\n, $token->as_string());
+
+=head1 DESCRIPTION
+
+Net::PMP::AuthToken is the object representation of an authorization token.
+
+=head1 METHODS
+
+=head2 access_token
+
+=head2 token_type
+
+=head2 token_issue_date
+
+=head2 token_expires_in
+
+=head2 expires_in
+
+Alias for B<token_expires_in>.
+
+=head2 as_string
+
+Returns the B<access_token>. Objects are overloaded to stringify with as_string().
 
 =head1 AUTHOR
 
